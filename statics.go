2e215c4ea35bc7b4ff921da31659e431749c5faf
Will Brown <will.h.brown@gmail.com>
2012-11-30 17:54:12 +1100

    Strip port off of host

--- statics.go
+++ statics.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-  "flag"
-  "net/http"
-  "strings"
+	"flag"
+	"net/http"
+	"strings"
 )
 
 var baseDirectory *string = flag.String("base", ".", "Base directory for files.")
 var port *int = flag.Int("port", 8080, "Port to listen for requests on.")
 
 func hostDirectory(r *http.Request) http.Dir {
-  return http.Dir(strings.Join(
-    []string{ *baseDirectory, strings.Replace(r.Host, "/", "", -1) }, "/"))
+	hostName := strings.Split(r.Host, ":")[0]
+	path := strings.Join(
+		[]string{*baseDirectory, strings.Replace(hostName, "/", "", -1)}, "/")
+	return http.Dir(path)
 }
 
 func hostDispatcher(w http.ResponseWriter, r *http.Request) {
-  handler := http.FileServer(hostDirectory(r))
-  handler.ServeHTTP(w, r)
+	handler := http.FileServer(hostDirectory(r))
+	handler.ServeHTTP(w, r)
 }
 
 func main() {
-  http.HandleFunc("/", hostDispatcher)
-  http.ListenAndServe(":8080", nil)
+	flag.Parse()
+	http.HandleFunc("/", hostDispatcher)
+	http.ListenAndServe(":8080", nil)
 }
